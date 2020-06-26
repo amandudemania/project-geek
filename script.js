b5d1974075b129a150bb40c0d0810e110e0fad82
amandudemania <67448116+amandudemania@users.noreply.github.com>
2020-06-25 22:51:45 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var importedAjax = [
+{
+    "lang": "javascript",
+    "DTVAR": "false",
+    "favlang": "true",
+    "type": "["functionality", "software"]"
+},
+{
+    "lang": "HTML",
+    "DTVAR": "NO-VAR",
+    "favlang": "false",
+    "type": ["content", "web-dvlp"]
+},
+{
+    "lang": "CSS",
+    "DTVAR": "NO-VAR",
+    "favlang": "false",
+    "type": ["design", "web-dvlp"]
+},
+{ 
+    "lang": "HTML",
+    "DTVAR": "NO-VAR",
+    "favlang": "false",
+    "type": ["content", "web-dvlp"]
+},
+{
+    "lang": "C#",
+    "DTVAR": "true",
+    "favlang": "false",
+    "type": ["functionality", "game-dvlp"]
+}
+];
